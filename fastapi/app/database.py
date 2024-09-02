9e1cd3c8d83ac6454ab6c3fe81033973ffdd5e19
miyanagas <zs0h8po2t7ad@gmail.com>
2024-09-03 01:12:18 +0900

    🔧  MySQLの接続設定を追加

---
+++ fastapi/app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+password = os.environ["MYSQL_ROOT_PASSWORD"]
+dbname = os.environ["MYSQL_DATABASE"]
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:{password}@mysql:3306/{dbname}"
+
+# DBと接続するエンジンの作成
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# DBとの論理的な接続であるセッションを作成
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()  # DBのテーブルに対応するクラスのベース
