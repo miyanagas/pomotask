3f203ca044cd480d58db92fa3fabf859288b43e1
miyanagas <zs0h8po2t7ad@gmail.com>
2024-09-19 02:32:35 +0900

    ✨ ToDoListをフィルタリングするコンポーネントを作成＆ToDoの完了をDBに連携する機能を追加

--- frontend/app/vue-app/src/App.vue
+++ frontend/app/vue-app/src/App.vue
@@ -9,6 +9,7 @@ const request = axios.create({
 const error = ref(null);
 const toDoList = ref([]);
 const newToDo = ref('');
+const isToDoFilter = ref(false);
 
 onMounted(() => {
     fetchToDoList()
@@ -16,7 +17,11 @@ onMounted(() => {
 
 const fetchToDoList = async () => {
     try {
-        const response = await request.get('/todolist')
+        const response = await request.get('/todolist', {
+          params: {
+            done_filter: isToDoFilter.value
+          }
+        })
 
         if (!response.data) return
         toDoList.value = response.data
@@ -49,6 +54,21 @@ const addToDo = async () => {
     fetchToDoList()
 }
 
+const updateToDo = async (toDo) => {
+  console.log(toDo)
+  try {
+    await request.post('/todolist/update',
+    {
+      id: toDo.id,
+      title: toDo.title,
+      is_done: toDo.is_done
+    })
+  } catch (e) {
+    error.value = e
+    alert('エラーが発生しました')
+  }
+}
+
 const deleteToDoList = async () => {
     try {
         await request.delete('/todolist')
@@ -74,11 +94,15 @@ const deleteToDoList = async () => {
       <input type="text" v-model="newToDo" placeholder="ToDoを入力してください">
       <button @click="addToDo()">追加</button>
     </div>
+    <div class="filter-todo">
+      <input @change="fetchToDoList()" class="filter-checkbox" type="checkbox" v-model="isToDoFilter">
+      <span>完了したToDoを非表示</span>
+    </div>
     <div class="todo-list">
           <ul  v-if="toDoList.length!==0">
               <li class="todo-item" v-for="(toDo, index) in toDoList" :key="index">
                   <span>{{ toDo.title }}</span>
-                  <span><input class="status-checkbox" type="checkbox" v-model="toDo.is_done"></span>
+                  <span><input @change="updateToDo(toDo)" class="status-checkbox" type="checkbox" v-model="toDo.is_done"></span>
               </li>
           </ul>
           <p v-else>ToDoがありません</p>
@@ -130,6 +154,7 @@ header {
   border: 1px solid #ccc;
   border-radius: 4px;
   margin: 2rem;
+  font-size: 16px;
 }
 
 .input-todo button {
@@ -140,6 +165,28 @@ header {
   background-color: hsla(160, 100%, 37%, 1);
   color: white;
   cursor: pointer;
+  font-size: 16px;
+}
+
+.filter-todo {
+  width: 55%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 0 auto;
+}
+
+.filter-todo span {
+  font-size: small;
+  padding: 0.25rem 0.5rem;
+}
+
+.filter-checkbox {
+  display: block;
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  accent-color: hsla(160, 100%, 37%, 1);
 }
 
 .todo-list {
@@ -160,9 +207,10 @@ header {
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
-  margin: 0.5rem 0;
+  margin: 0.125rem 0;
   border: 1px solid #ccc;
   border-radius: 4px;
+  cursor: pointer;
 }
 
 .status-checkbox {
@@ -170,7 +218,7 @@ header {
   width: 25px;
   height: 25px;
   cursor: pointer;
-  accent-color: hsla(160, 100%, 37%, 1);
+  accent-color: royalblue;
 }
 
 .delete-todo {
@@ -185,9 +233,10 @@ header {
   padding: 0.5rem 1rem;
   border: 1px solid #ccc;
   border-radius: 4px;
-  background-color: hsla(160, 100%, 37%, 1);
+  background-color:red;
   color: white;
   cursor: pointer;
+  font-size: 16px;
 }
 
 /*
