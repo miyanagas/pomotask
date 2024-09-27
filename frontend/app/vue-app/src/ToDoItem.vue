f510007430dcfff694b42eb051cfdb8a0c706d2a
miyanagas <zs0h8po2t7ad@gmail.com>
2024-09-27 16:55:33 +0900

    ✨ youtube再生機能を追加

--- frontend/app/vue-app/src/ToDoItem.vue
+++ frontend/app/vue-app/src/ToDoItem.vue
@@ -26,6 +26,8 @@ let currentTask = 0;
 const circumference = 2 * Math.PI * 45;
 const remainingTime = ref(0);
 const isTimerRunning = ref(false);
+const isMenuOpen = ref(false);
+const youtubeUrl = ref("");
 
 onMounted(() => {
   currentTask = taskPlan[taskIndex];
@@ -86,6 +88,26 @@ const getNextTask = () => {
   if (taskIndex >= taskPlan.length) taskIndex = 0;
   return taskPlan[taskIndex];
 };
+
+const getYouTubeId = (url) => {
+  const regExp =
+    /^(https:\/\/www\.youtube\.com\/watch\?v=|https:\/\/youtu\.be\/)([a-zA-Z0-9-_]+)(\?|$)/;
+  const match = url.match(regExp);
+  return match ? match[2] : null;
+};
+
+const roadYouTube = () => {
+  const youtubeId = getYouTubeId(youtubeUrl.value);
+  youtubeUrl.value = "";
+  if (youtubeId) {
+    const iframe = document.querySelector("iframe");
+    iframe.width = "640";
+    iframe.height = "360";
+    iframe.src = `https://www.youtube.com/embed/${youtubeId}`;
+  } else {
+    alert("無効なURLです");
+  }
+};
 </script>
 
 <template>
@@ -102,6 +124,38 @@ const getNextTask = () => {
         height="35"
       />
       <h1 class="todo-title">{{ $route.params.title }}</h1>
+      <button v-if="!isMenuOpen" @click="isMenuOpen = !isMenuOpen">
+        <img
+          id="toggle-button"
+          src="./assets/arrow-down.svg"
+          width="25"
+          height="25"
+        />
+      </button>
+      <button v-else @click="isMenuOpen = !isMenuOpen">
+        <img
+          id="toggle-button"
+          src="./assets/arrow-up.svg"
+          width="25"
+          height="25"
+        />
+      </button>
+    </div>
+    <div v-show="isMenuOpen" class="video-container">
+      <div class="video-input-form">
+        <input type="text" v-model="youtubeUrl" placeholder="YouTube URL" />
+        <button @click="roadYouTube()">ロード</button>
+      </div>
+      <iframe
+        width="0"
+        height="0"
+        src=""
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
     </div>
     <div class="todo-timer">
       <div class="timer-screen">
@@ -169,6 +223,14 @@ const getNextTask = () => {
   font-weight: bold;
 }
 
+.todo-title-headline button {
+  margin: 0 0 0 auto;
+  padding: 0.5rem;
+  background-color: #f1f1f1;
+  border: none;
+  cursor: pointer;
+}
+
 .todo-timer {
   width: 55%;
   margin: 0.5rem auto;
@@ -189,7 +251,42 @@ const getNextTask = () => {
   font-family: "Lucida Console", monospace;
   margin: 8px 8px 8px 64px;
   padding: 8px;
-  transition: 0.3s;
+}
+
+.video-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem auto;
+  width: 55%;
+}
+
+.video-input-form {
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  margin: 0rem auto 1rem auto;
+}
+
+.video-input-form input {
+  width: 60%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin: 0 0.5rem 0 0;
+  font-size: 14px;
+}
+
+.video-input-form button {
+  padding: 0.5rem 1rem;
+  margin: 0 0 0 0.5rem;
+  border: 1px solid black;
+  border-radius: 4px;
+  background-color: red;
+  color: white;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: bold;
 }
 
 .progress-circle {
