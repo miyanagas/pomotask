9b843ca01dab921ba67e3cb5bfbc48722b8fab6e
miyanagas <zs0h8po2t7ad@gmail.com>
2024-11-11 00:39:14 +0900

    ✨ ログインと登録ページのルーティング追加 #16

--- frontend/app/vue-app/src/router.js
+++ frontend/app/vue-app/src/router.js
@@ -1,9 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
 
+import LogInView from "./components/LogIn.vue";
+import SignUpView from "./components/SignUp.vue";
 import ToDoListView from "./components/ToDoList.vue";
 import ToDoItemView from "./components/ToDoItem.vue";
 
 const routes = [
+  { path: "/login", component: LogInView, meta: { hideHeader: true } },
+  { path: "/signup", component: SignUpView, meta: { hideHeader: true } },
   { path: "/", component: ToDoListView },
   { path: "/todo_list/:id/:title", name: "todo_item", component: ToDoItemView },
 ];
