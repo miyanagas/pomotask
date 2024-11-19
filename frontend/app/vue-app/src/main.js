03be08af7db56ba83b878ddbce3f24bbf59c1b80
miyanagas <zs0h8po2t7ad@gmail.com>
2024-11-19 18:03:30 +0900

    ✨ Piniaの初期化を追加 #45

--- frontend/app/vue-app/src/main.js
+++ frontend/app/vue-app/src/main.js
@@ -1,7 +1,8 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).mount("#app");
