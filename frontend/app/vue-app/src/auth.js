8c4204c21ba89ff97a31b18976d8d3abfbaf9519
miyanagas <zs0h8po2t7ad@gmail.com>
2024-11-19 17:56:18 +0900

    ✨ ログイン状態を管理 #45

--- frontend/app/vue-app/src/auth.js
+++ frontend/app/vue-app/src/auth.js
@@ -1,11 +1,21 @@
-const isAuthenticated = () => {
-  const token = localStorage.getItem("access_token");
+import { defineStore } from "pinia";
+import requestAPI from "./components/requestAPI";
 
-  if (token) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export default isAuthenticated;
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    isAuthenticated: false,
+  }),
+  actions: {
+    async checkLoginStatus() {
+      try {
+        const response = await requestAPI.get("/auth/status/", {
+          withCredentials: true,
+        });
+        this.isAuthenticated = response.data.is_authenticated;
+      } catch (error) {
+        console.error("Error checking login status:", error);
+        this.isAuthenticated = false;
+      }
+    },
+  },
+});
